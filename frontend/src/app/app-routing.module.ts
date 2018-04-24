59216a71b589c65140804eed3b23603ab15fd5fb
qimingxu <xuqiming422@outlook.com>
2018-04-24 13:07:46 -0700

    add relative link

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -7,11 +7,11 @@ import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
 const routes: Routes = [
     { path: '', loadChildren: './layout/layout.module#LayoutModule', canActivate: [AuthGuard] },
     { path: 'login', loadChildren: './login/login.module#LoginModule' },
-    { path: 'signup', loadChildren: './signup/signup.module#SignupModule' },
-    { path: 'error', loadChildren: './server-error/server-error.module#ServerErrorModule' },
-    { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
-    { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
-    { path: '**', redirectTo: 'not-found' }
+    { path: 'signup', loadChildren: './signup/signup.module#SignupModule' }
+    // { path: 'error', loadChildren: './server-error/server-error.module#ServerErrorModule' },
+    // { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
+    // { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+    // { path: '**', redirectTo: 'not-found' }
 ];
 
 @NgModule({
