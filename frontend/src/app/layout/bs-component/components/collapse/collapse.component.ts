381d2e985392d9442b7467d7fe9903ca22ab0c5b
Christine <Christine@guos-MacBook-Air.local>
2018-04-24 11:34:34 -0700

    courses

--- frontend/src/app/layout/bs-component/components/collapse/collapse.component.ts
+++ frontend/src/app/layout/bs-component/components/collapse/collapse.component.ts
@@ -5,6 +5,8 @@ import { routerTransition } from '../../../../router.animations';
 import { DataService } from '../../../../data.service';
 import { RestApiService } from '../../../../rest-api.service';
 import {HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+
 
 @Component({
     selector: 'app-collapse',
@@ -14,12 +16,12 @@ import {HttpClient} from '@angular/common/http';
 })
 export class CollapseComponent  implements OnInit {
 
-public Courses = [{name: 'Algorithm', instructor: 'Phile', number: 'CS5800',
-    description: 'a good class', credit: '2', semester: 'Spring',
-reviews: 'here', isCollapsed: true},
-    {name: 'Algorithm', instructor: 'Phile', number: 'CS5800',
-        description: 'a good class', credit: '2', semester: 'Spring',
-        reviews: 'here', isCollapsed: true}];
+// public Courses = [{name: 'Algorithm', instructor: 'Phile', number: 'CS5800',
+//     description: 'a good class', credit: '2', semester: 'Spring',
+// reviews: 'here', isCollapsed: true},
+//     {name: 'Algorithm', instructor: 'Phile', number: 'CS5800',
+//         description: 'a good class', credit: '2', semester: 'Spring',
+//         reviews: 'here', isCollapsed: true}];
     //
     // name: { type: String, unique: true },
     // instructor: String,
@@ -29,7 +31,8 @@ reviews: 'here', isCollapsed: true},
     // semester: { type: Schema.Types.ObjectId, ref: 'Semester'},
     // reviews: [{ type: Schema.Types.ObjectId, ref: 'Review'}],
     // created: {
-
+    public courses : any[];
+    dataget = {};
     constructor(
         private router: Router,
         private rest: RestApiService,
@@ -38,10 +41,47 @@ reviews: 'here', isCollapsed: true},
     ) {}
 
     ngOnInit() {
-        // this.RestApiService.getCourses()
-        //     .subscribe(
-        //      Courses => this.Courses = Courses,
-        //         error =>  this.errorMessage = <any>error);
+
+// let self =this;
+//
+//          this.rest.get(
+//             'http://localhost:3030/api/courses'
+//         ).then(data => this.courses = data['courses'] );
+//
+// console.log(this.courses);
+//
+//     }
+
+
+        this.rest.get(
+            'http://localhost:3030/api/courses'
+        ).then( function (this, res){
+           this.courses =  res['courses'];
+        },function (err){});
+
+        console.log(this.courses);
+        // console.log(dataget)
+
+        }
+
+
+
+
+
+        // get(link: string) {
+        //     return this.http.get(link, { headers: this.getHeaders() }).toPromise();
+        // }
+            // const data = await this.rest.post(
+            // 'http://localhost:3030/api/accounts/login',
+            // {
+            //     email: this.email,
+            //     password: this.password,
+            // },
+        // this.http.get(link, { headers: this.getHeaders() }).toPromise();
+            // .getCourses()
+            // .subscribe(
+            //  Courses => this.Courses = Courses,
+            //     error =>  this.errorMessage = <any>error);
     }
 
-}
+
