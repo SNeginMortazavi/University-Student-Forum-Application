012f77d88282376c822be6f5cecd05d3773ac736
Christine <Christine@guos-MacBook-Air.local>
2018-04-24 21:54:10 -0700

    here

--- frontend/src/app/layout/form/form.component.ts
+++ frontend/src/app/layout/form/form.component.ts
@@ -3,6 +3,9 @@ import { routerTransition } from '../../router.animations';
 // import { DataService } from ' ../../data.service';
 // import { RestApiService } from '../../rest-api.service';
 import {Router} from '@angular/router';
+import {DataService} from "../../data.service";
+import {HttpClient} from "@angular/common/http";
+import {RestApiService} from "../../rest-api.service";
 
 @Component({
     selector: 'app-form',
@@ -11,17 +14,48 @@ import {Router} from '@angular/router';
     animations: [routerTransition()]
 })
 export class FormComponent implements OnInit {
-    public Courses = [{}];
-    public Semesters = [{name: '2017 Spring'},
-        {name: '2017 Summer'}
-    ];
+    public Courses : any[];
+    public Semesters : any[];
+    public semester:any;
     public Title = '';
     public Description = '';
-    constructor( ) {
+    constructor(private router: Router,
+                private rest: RestApiService,
+                private data: DataService,
+                private http: HttpClient ) {
   }
 
-    ngOnInit() {
-        // this.Courses = this.rest.get(
-        //     'http://localhost:3030/aping/courses') ;
+    async ngOnInit() {
+        try {
+            const data = await this.rest.get(
+                'http://localhost:3030/api/semesters'
+            );
+            data['success']? (this.Semesters = data['semesters']):this.data.error(data['message']);
+
+        }catch(error){
+            this.data.error(error['message']);
+            console.log('error');
+
+        }
+    }
+
+    setSemester(semObj:any){
+        this.semester= semObj;
+        this.getCourses(semObj);
+    }
+
+    async getCourses(semObj:any){
+        try {
+            const data = await this.rest.get(
+                'http://localhost:3030/api/semesters/' + semObj._id
+            );
+            data['success']? (this.Courses = data['courses']):this.data.error(data['message']);
+
+
+        }catch(error){
+            this.data.error(error['message']);
+            console.log('error');
+
+        }
     }
 }
